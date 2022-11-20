2d48d0284ee2a78c75f0bba29163d70329ca26bc
alif-hanafiah <muhalifhanafiah@gmail.com>
2022-11-21 01:13:45 +0700

    Change karma configuration in karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,6 @@
 // Generated on Fri Jul 03 2020 20:15:52 GMT+0700 (Western Indonesia Time)
 module.exports = function (config) {
   config.set({
-
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
@@ -11,9 +10,7 @@ module.exports = function (config) {
     frameworks: ['jasmine'],
 
     // list of files / patterns to load in the browser
-    files: [
-      'specs/**/*Spec.js',
-    ],
+    files: ['specs/**/*Spec.js'],
 
     // list of files / patterns to exclude
     exclude: [],
@@ -60,11 +57,11 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
